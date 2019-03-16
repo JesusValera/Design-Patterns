2c0b12ef7124b75de52341c8fd220b0aae790369
Jesus Valera <jesus1902@outlook.com>
2019-03-16 14:32:14 +0100

    Replace forEach method by stream().map{ ... }

--- src/CreationalPatterns/Builder/Vehicle.kt
+++ src/CreationalPatterns/Builder/Vehicle.kt
@@ -1,5 +1,7 @@
 package CreationalPatterns.Builder
 
+import java.util.stream.Collectors
+
 class Vehicle {
 
     private val cars: ArrayList<Car> = ArrayList()
@@ -13,22 +15,20 @@ class Vehicle {
     }
 
     fun getInfo(): String {
-        var result = ""
-        cars.forEach {
-            result += String.format("Brand: %s\n" +
-                    "Color: %s\n" +
-                    "Are windows tinted? %b\n" +
-                    "Number of seats are %d\n" +
-                    "Fuel type: %s\n" +
-                    "\t+ Calorific value: %.2f\n" +
-                    "\t+ Ignition temperature: %.3f\n" +
-                    "\t+ Moisture content: %d" +
-                    "\n==================================\n",
-                    it.javaClass.simpleName, it.color, it.windowTinted,
-                    it.seat, it.fuel.javaClass.simpleName, it.fuel.calorificValue,
-                    it.fuel.ignitionTemperature, it.fuel.moistureContent)
-        }
-
-        return result
+        return cars.stream()
+                .map {
+                    String.format("Brand: %s\n" +
+                            "Color: %s\n" +
+                            "Are windows tinted? %b\n" +
+                            "Number of seats are %d\n" +
+                            "Fuel type: %s\n" +
+                            "\t+ Calorific value: %.2f\n" +
+                            "\t+ Ignition temperature: %.3f\n" +
+                            "\t+ Moisture content: %d" +
+                            "\n==================================\n",
+                            it.javaClass.simpleName, it.color, it.windowTinted,
+                            it.seat, it.fuel.javaClass.simpleName, it.fuel.calorificValue,
+                            it.fuel.ignitionTemperature, it.fuel.moistureContent)
+                }.collect(Collectors.joining())
     }
 }
